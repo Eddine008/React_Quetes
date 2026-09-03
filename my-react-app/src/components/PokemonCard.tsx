a8cfd836049526dd280c5d431329058ca77d087a
Eddinee <edssee008@gmail.com>
2026-09-03 16:49:00 +0200

    changement de la balise PokemonCard

--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,8 +1,23 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  }, 
+];
+
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return <figure>
+        if 
+        <img src={pokemon.imgSrc} alt={pokemon.name}/>
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre" />
-        <figcaption>Bulbasaur</figcaption>
+        <figcaption>{pokemon.name}</figcaption>
             
-    </figure>
+    </figure> 
 }
 export default PokemonCard
