a1b7390158c433198e8d4d02af7d9ff721939367
Eddinee <edssee008@gmail.com>
2026-09-06 22:46:56 +0200

    Mise au propre

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-
 import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,18 +1,19 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  }, 
-];
-
 interface Pokemon {
-  name : string;
+  name: string;
   imgSrc?: string;
+}
 
+function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+  return (
+    <figure>
+      {pokemon.imgSrc != null ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
 }
 
 export default PokemonCard;
